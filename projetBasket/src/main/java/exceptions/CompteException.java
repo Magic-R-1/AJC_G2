9aa19b68d95af6443732f3fd23b7e0f23cc5a09b
LeaSimonet <simonetlea97@gmail.com>
2023-05-09 16:26:57 +0200

    Ajout de toutes les exceptions

---
+++ projetBasket/src/main/java/exceptions/CompteException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+public class CompteException extends RuntimeException{
+	
+	
+	public CompteException() {
+	}
+	
+	public CompteException(String message) {
+		super(message);
+	}
+
+}
---
+++ projetBasket/src/main/java/exceptions/PersonneException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+public class PersonneException  extends RuntimeException{
+	
+	
+	public PersonneException() {
+	}
+	
+	public PersonneException(String message) {
+		super(message);
+	}
+
+}
