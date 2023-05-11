16f319c839ba9c34d0f61932dde6bfe5926d56e3
bernadette <bernadettemensanh@gmail.com>
2023-05-12 00:38:07 +0200

    propre

--- projetBasket/src/test/java/test/ArbitreServiceTest.java
+++ projetBasket/src/test/java/test/ArbitreServiceTest.java
@@ -1,7 +1,5 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
