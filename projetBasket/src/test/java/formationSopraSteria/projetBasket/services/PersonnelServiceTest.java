1effa3d9770f241d161163e0b934172400341323
olivier gozlan <ybrahimaelhafid@gmail.com>
2023-06-02 09:41:44 +0200

    personnel modifié

--- projetBasket/src/test/java/formationSopraSteria/projetBasket/services/PersonnelServiceTest.java
+++ projetBasket/src/test/java/formationSopraSteria/projetBasket/services/PersonnelServiceTest.java
@@ -1,8 +1,11 @@
 package formationSopraSteria.projetBasket.services;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import formationSopraSteria.projetBasket.entities.Personnel;
 
 @SpringBootTest
 class PersonnelServiceTest {
@@ -10,7 +13,21 @@ class PersonnelServiceTest {
 		@Autowired
 		PersonnelService personnelSrv;
 		
+		
 		@Test
+		   void testCreate() {
+	     
+		  
+	       Personnel personnel = new Personnel(1L,"afi","dd",2000,null,null);
+	       personnelSrv.create(personnel);
+	       
+	       Personnel personnel1 = new Personnel(2L,"ani","ddi",2000,null,null);
+	       personnelSrv.create(personnel1);
+
+			}
+		
+		@Test
+		@Disabled
 		void testUpdateSalaire() {
 			personnelSrv.modifierSalairePersonnel(1L, 15200);
 		}
--- projetBasketAngular/src/app/model/personnel.ts
+++ projetBasketAngular/src/app/model/personnel.ts
@@ -3,11 +3,11 @@ import { Equipe } from './equipe';
 
 export class Personnel {
   constructor(
-    public id?: number,
     public nom?: string,
     public prenom?: string,
     public salaire?: number,
     public equipe?: Equipe,
-    public role_personnel?: RolePersonnel
+    public role_personnel?: RolePersonnel,
+    public id?: number
   ) {}
 }
